e548c928d78b7ea9139bd13aabac3eb01e98cd59
Rafi Ramadhana <ramadhanarafi437@gmail.com>
2022-11-08 13:21:17 +0700

    node package manager

--- nodejs-basic/app.js
+++ nodejs-basic/app.js
@@ -1,6 +1,11 @@
-const coffee = require('./coffee');
-const { firstName, lastName } = require('./user');
+// const coffee = require('./coffee');
+// const { firstName, lastName } = require('./user');
 
-console.log(coffee);
-console.log(firstName);
-console.log(lastName);
+// console.log(coffee);
+// console.log(firstName);
+// console.log(lastName);
+
+// const moment = require('moment');
+
+// const date = moment().format("MMM Do YY");
+// console.log(date);
