efd31f15f8aa79e7688e98b8dcc8f4c983684151
Rafi Ramadhana <ramadhanarafi437@gmail.com>
2022-11-09 12:18:24 +0700

    membuat http server

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -1,1 +1,20 @@
-console.log('Halo, kita akan belajar membuat server');
+const http = require('http');
+
+const requestListener = (request, response) => {
+    response.setHeader('Content-Type', 'text/html');
+
+    response.statusCode = 200;
+    // response.send('<h1>Halo HTTP Server!</h1>');
+    res.write('<html><body><p>Hello world!</p></body></html>');
+    res.end();
+};
+
+
+const server = http.createServer(requestListener);
+
+const port = 5000;
+const host = 'localhost';
+
+server.listen(port, host, () => {
+    console.log(`Server berjalan pada http://${host}:${port}`);
+});
