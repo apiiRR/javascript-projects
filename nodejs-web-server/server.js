de7f9fdaa779864d757e634ebca8a545fe689b96
Rafi Ramadhana <ramadhanarafi437@gmail.com>
2022-11-11 19:36:23 +0700

    routing request

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -4,24 +4,59 @@ const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
     response.statusCode = 200;
 
-    const { method } = request;
+    const { method, url } = request;
 
-    if (method === 'GET') {
-        response.end('<h1>Hello!</h1>');
-    }
+    // if (method === 'GET') {
+    //     response.end('<h1>Hello!</h1>');
+    // }
 
-    if (method === 'POST') {
-        let body = [];
+    // if (method === 'POST') {
+    //     let body = [];
 
-        request.on('data', (chunk) => {
-            body.push(chunk);
-        });
+    //     request.on('data', (chunk) => {
+    //         body.push(chunk);
+    //     });
 
-        request.on('end', () => {
-            body = Buffer.concat(body).toString();
-            const { name } = JSON.parse(body);
-            response.end(`<h1>Hai, ${name}!</h1>`);
-        });
+    //     request.on('end', () => {
+    //         body = Buffer.concat(body).toString();
+    //         const { name } = JSON.parse(body);
+    //         response.end(`<h1>Hai, ${name}!</h1>`);
+    //     });
+    // }
+
+    if (url === '/') {
+        switch (method) {
+            case 'GET':
+                response.end('<h1> Ini adalah halaman homepage! </h1>');
+                break;
+            default:
+                response.end('<h1> Halaman tidak dapat diakses dengan <any> request! </h1>');
+                break;
+        }
+    } else if (url === '/about') {
+        switch (method) {
+            case 'GET':
+                response.end('<h1> Halo ini adalah halaman about! </h1>');
+                break;
+            case 'POST':
+                let body = [];
+
+                request.on('data', (chunk) => {
+                    body.push(chunk);
+                });
+
+                request.on('end', () => {
+                    body = Buffer.concat(body).toString();
+                    const { name } = JSON.parse(body);
+                    response.end(`<h1> Halo, ${name}! Ini adalah halaman about </h1>`);
+                });
+                break;
+            default:
+                response.end(`<h1> Halaman tidak dapat diakses dengan ${method} request! </h1>`);
+                break;
+        }
+    } else {
+        response.end('<h1> Halaman tidak ditemukan!</h1>');
     }
 };
 
