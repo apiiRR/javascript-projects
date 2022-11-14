50bb2c2268cfa0032678332b9fea424a8291ef77
Rafi Ramadhana <ramadhanarafi437@gmail.com>
2022-11-14 17:15:34 +0700

    menyimpan catatan

---
+++ notes-app-back-end/src/handler.js
@@ -0,0 +1,20 @@
+const { nanoid } = require('nanoid');
+const notes = require('./notes');
+
+const addNoteHandler = (request, h) => {
+  const { title, tags, body } = request.payload;
+
+  const id = nanoid(16);
+  const createdAt = new Date().toISOString();
+  const updateAt = createdAt;
+
+  const newNote = {
+    title, tags, body, id, createdAt, updateAt,
+  };
+
+  notes.push(newNote);
+
+  const isSuccess = notes.filter((note) => note.id === id).length > 0;
+};
+
+module.exports = { addNoteHandler };
