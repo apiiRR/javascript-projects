80efb70c2b264a56f51f93747a3740bce53513a6
Rafi Ramadhana <ramadhanarafi437@gmail.com>
2022-11-14 17:32:54 +0700

    same origin policy

--- notes-app-back-end/src/server.js
+++ notes-app-back-end/src/server.js
@@ -5,6 +5,11 @@ const init = async () => {
   const server = Hapi.server({
     port: 8888,
     host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
   });
 
   server.route(routes);
